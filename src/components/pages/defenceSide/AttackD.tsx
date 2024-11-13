19c40e31391cce57a9b9a06edc58d0a9d2d10cce
yonatan957 <yonatan22660@gmail.com>
2024-11-13 15:32:57 +0200

    add logic of show his interceptors on attackD

--- src/components/pages/defenceSide/AttackD.tsx
+++ src/components/pages/defenceSide/AttackD.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useAppSelector } from '../../../redux/store';
 import { IAttack } from '../../../types/attack'
+import missles from '../missiles.json'
 
 interface Props {
     attack: IAttack
 }
 export default function AttackD({attack}:Props) {
+    const user = useAppSelector((state) => state.user.user);
+    const [activeIntercptors, setActiveIntercptors] = useState<string[]>(((): string[]=>{
+        const intercepts:string[] = []
+        user?.resources.forEach((resource) => {
+            if(attack.intercepts.includes(resource.name)){
+                intercepts.push(resource.name)
+            }
+        })
+        return intercepts
+    })()); 
+    useEffect(() => {        
+        setActiveIntercptors(Intercptors=>{
+            return Intercptors.filter((interceptor) => {
+                return (attack.tymeToHit < missles.find(missle => missle.name == interceptor)!.speed)
+            })
+        })
+    }, [attack.tymeToHit])
   return (
     <div>
       <h3>{attack.name}</h3>
       {!attack.id_intercepted && <h4>Time to hit:{attack.tymeToHit}</h4>}
+      {attack.id_intercepted && attack.id_intercepted == user?._id ? <h4>Intercepted by you</h4> : <h4>Intercepted by {attack.intercepts}</h4>}   
+      {activeIntercptors.map((interceptor) => (<button key={interceptor}>Lounch {interceptor}</button>))}   
     </div>
   )
 }
