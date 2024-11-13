448512edc4f99c6cd7d57ec14ca90e640a7dfe5d
yonatan957 <yonatan22660@gmail.com>
2024-11-13 12:01:39 +0200

    create thunk in user slice with extra reducers and connect main to the store

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <Provider store={store}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </Provider>
+  </BrowserRouter>
 )
--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,5 +1,6 @@
-import { ActionReducerMapBuilder, createSlice } from "@reduxjs/toolkit";
+import { ActionReducerMapBuilder, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { DataStatus, userState } from "../../types/redux";
+import { IUser } from "../../types/user";
 
 const initialState: userState = {
     error: null,
@@ -7,14 +8,49 @@ const initialState: userState = {
     user: null,
 };
 
-
+export const fetchLogin = createAsyncThunk('user/login',
+    async (userData: { userName: string, password: string, }, thunkApi) => {
+        try {
+            const res = await fetch(import.meta.env.VITE_API_URL + '/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
+            if (!res.ok) {
+                thunkApi.rejectWithValue("can't login, please try again");
+            }
+            const data = await res.json();
+            thunkApi.fulfillWithValue(data);
+            localStorage.setItem('token', data.token)
+            return data
+        } catch (error) {
+            thunkApi.rejectWithValue(error instanceof Error ? error.message : "Unknown error occurred");
+        }
+    }
+);
 
 const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {},
     extraReducers: (builder:ActionReducerMapBuilder<userState>) => {
-
+        builder.addCase(fetchLogin.pending, (state) => {
+            state.status = DataStatus.LOADING;
+            state.error = null;
+            state.user = null;
+        })
+        .addCase(fetchLogin.fulfilled, (state, action) => {
+            state.status = DataStatus.SUCCESS;
+            state.error = null;
+            state.user = action.payload as IUser;
+        })
+        .addCase(fetchLogin.rejected, (state, action) => {
+            state.status = DataStatus.FAILED;
+            state.error = action.payload as string;
+            state.user = null;
+        })
     }
 })
 
