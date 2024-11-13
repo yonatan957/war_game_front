c71783f950c82d96db3e74ae1fb6ee93f2b93127
yonatan957 <yonatan22660@gmail.com>
2024-11-13 17:20:00 +0200

    add socket

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
+import { io } from 'socket.io-client'
+
+export const socket = io(`http://localhost:3030`)
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
